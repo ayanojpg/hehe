332c0bf044bd74232cabe3e86165110d3bd5d3ca
ayanojpg <0966463231jennychen@gmail.com>
2025-12-31 15:20:19 +0800

    asdn

--- Public/js/bs5vue.js
+++ Public/js/bs5vue.js
@@ -1,26 +1,4 @@
-//vue 2.0
 
-// var myObject=new Vue({
-//     el: "#portfolio",
-//     data: {Portfolio: []}
-//     })
-//     $.ajax({
-//         url:"/portfolio",
-//         method: "get",
-//         dataType: "json",
-//         success: function(data){
-//             myObject.Portfolio = data;
-//         }
-//     })
-
-//     var vueService = new Vue({
-//         el: "#services",
-//         data: {Services:[
-//            {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
-//            {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
-//            {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
-//         ]}
-//     })
 const { createApp, ref } = Vue;
 
 var serviceApp=createApp({
@@ -31,7 +9,7 @@ var serviceApp=createApp({
     }
 }).mount("#showArt");
 
-//req services data fromm server
+
 $.ajax({
     url:"/art",
     method:"get",
